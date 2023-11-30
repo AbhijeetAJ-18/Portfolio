119907aa58d632ad5c4c69dc8b09bb36aacd7aa6
Abhijeet Jadhav <150771079+AbhijeetAJ-18@users.noreply.github.com>
2023-11-30 19:00:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+
+// Fileter Able Project Gallery
+window.addEventListener('load',()=>{
+    let allTab = document.querySelectorAll('.tab');
+    let allproject = document.querySelectorAll('.project');
+
+    // Remove-TabActive
+    const removeActive=()=> allTab.forEach(tab=>{
+        tab.classList.remove('tab-active')
+    })
+    allTab.forEach(tab=>{
+        // Add-TabActive
+        tab.addEventListener('click',(event)=>{
+            removeActive();
+            tab.classList.add('tab-active');
+
+          let filterName = event.target.getAttribute('data-name');
+
+          allproject.forEach(project=>{
+            let projectName = project.getAttribute('data-name');
+            if((projectName == filterName) || filterName == 'All'){
+                project.style.display= 'block';
+            }else{
+                project.style.display= 'none';
+            }
+          })
+
+        });
+    })
+});
+
+//  Swiper JS..............
+
+let swiper = new Swiper('.mySwiper',{
+    slidesPerView: 3,
+    loop: true,
+    spaceBetween: 20,
+
+    breakpoints:{
+        0:  {
+            slidesPerView: 1,
+        }, 
+        600:  {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        }, 
+        1024:  {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        }, 
+    },
+    pagination:{
+        el: '.swiper-pagination',
+        clickable: true,
+    }
+});
+
+// Nav scroll........
+
+window.addEventListener('scroll',()=>{
+    let nav = document.querySelector('nav');
+
+    nav.classList.toggle('nav-scroll', window.scrollY>0);
+});
+
+// Typed Js..........
+let typed = new Typed('.typed-text',{
+    strings: ['Web Developer','Apps Developer'],
+    typeSpeed: 100,
+    backspeed: 50,
+    backDalay: 1000,
+    loop: true,
+});
+
+// Menu Bar JS.......
+let menuBar  = document.querySelector('.menu-bar');
+let menuButton = document.querySelector('.menu-button');
+
+menuButton.addEventListener('click',()=>{
+    menuBar.classList.toggle('menu-none')
+})
+
